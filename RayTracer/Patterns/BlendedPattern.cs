31e5aad98f23b812adc550fcc0be5ec2e9ce11fb
Andy <28946117+DatHydroGuy@users.noreply.github.com>
2021-07-03 10:00:16 +0100

    Refactored BlendedPatterns

--- RayTracer/Patterns/BlendedPattern.cs
+++ RayTracer/Patterns/BlendedPattern.cs
@@ -1,22 +1,10 @@
-using System;
-
-namespace RayTracer
+namespace RayTracer.Patterns
 {
     public class BlendedPattern : Pattern
     {
-        private Pattern _patternA;
-        private Pattern _patternB;
+        public Pattern PatternA { get; set; }
 
-        public Pattern PatternA
-        {
-            get { return _patternA; }
-            set { _patternA = value; }
-        }
-        public Pattern PatternB
-        {
-            get { return _patternB; }
-            set { _patternB = value; }
-        }
+        public Pattern PatternB { get; set; }
 
         public BlendedPattern(Pattern patternA, Pattern patternB): base(null, null)
         {
@@ -29,13 +17,13 @@ namespace RayTracer
             var other = obj as BlendedPattern;
 
             return base.Equals(other) &&
-                PatternA == other.PatternA &&
-                PatternB == other.PatternB;
+                PatternA == other?.PatternA &&
+                PatternB == other?.PatternB;
         }
 
         public override int GetHashCode()
         {
-            return (int)(PatternA.GetHashCode() * 5 + PatternB.GetHashCode() * 3 + base.GetHashCode() * 2);
+            return PatternA.GetHashCode() * 5 + PatternB.GetHashCode() * 3 + base.GetHashCode() * 2;
         }
 
         public override Colour ColourAtPoint(Point targetPoint)
